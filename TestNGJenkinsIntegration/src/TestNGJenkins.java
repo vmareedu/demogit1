4f34023a0e82d2612287b70e6a6dcf30cba07e34
venkat.mareedu <venkat.mareedu@cgi.com>
2017-04-25 18:32:48 -0500

    Added Files

---
+++ TestNGJenkinsIntegration/src/TestNGJenkins.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+public class TestNGJenkins {
+	@Test
+	public void jenkins_TestNg()
+	{
+		System.out.println("Jenkins Integration with TestNG is success");
+	}
+
+}
